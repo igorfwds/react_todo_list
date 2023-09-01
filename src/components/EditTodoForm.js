50890040a1fc38f0447a31501dad214b28e95180
igorfwds <igorfwds@hotmail.com>
2023-09-01 19:58:17 -0300

    Adição das funcionalidades

---
+++ src/components/EditTodoForm.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react'
+
+export const EditTodoForm = ({editTodo, task}) => {
+    const [value, setValue] = useState("")
+    const handleSubmit= e =>{
+        e.preventDefault();
+
+        editTodo(value, task.id);
+        setValue("")
+
+    }
+
+  return (
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type='text' className='todo-input' value={value} placeholder='Atualize sua tarefa aqui' onChange={(e) => setValue(e.target.value)} />
+        <button type='submit' className='todo-btn'>Atualizar Tarefa</button>
+    </form>
+  )
+}
